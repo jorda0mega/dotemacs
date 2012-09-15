33cc8711f52f7a6d231ce54446a5e41018621fad
jorda0mega <jorda0mega@gmail.com>
2012-09-15 00:42:52 -0400

    wb line numbers added

--- init.el
+++ init.el
@@ -46,8 +46,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (bubbleberry)))
- '(custom-safe-themes (quote ("f87e6316b1e6cfc1ba2ade7225f0bb1a3a63e43a0ced927c135eb8bb48361375" "92ffa62354bf270b2526bf541eb43eb4a471bb8a3f4f3d2f67052079995069e5" "586e5bb76cc42e33684c69a12930df656d33c8d19dc7c6d5970fdd25ea069b42" "668c57f968cf52b9fed3f11c1c7fb2b0497e6f4c126ecc4e9dd3480b828af803" "d2ad04cdf7af01a803f0dd1b399f3cf0904688121f8a680f7a1af3935f4f1625" "9aeb193fa1626559e224e9a754777579a53ac71b4cddd2b8d2b6ef9adbb37754" default)))
+ '(Linum-format "%7i ")
+ '(ansi-color-names-vector ["#110F13" "#d13120" "#81af34" "#deae3e" "#7e9fc9" "#a878b5" "#7e9fc9" "#dcdddd"])
+ '(ansi-term-color-vector [unspecified "#110F13" "#d13120" "#81af34" "#deae3e" "#7e9fc9" "#a878b5" "#7e9fc9" "#dcdddd"])
+ '(custom-enabled-themes (quote (MechanicalTurq)))
+ '(custom-safe-themes (quote ("256253eb216b88e7d38bb75bff6dfb67b18fecb0c0172b46fbba1e92f64d9d6a" "4e5b55da54f821462d30ef10c9d28609a5f07aabfb731ae597daaffa8c344268" "5568bfc5f42014fa70709d157cd3489d042442912b470ef8afe06d0af29131d0" "27713778ce0291c1002fac10ba08f6de8430a6f24a506b821293b9eda806dfcc" "f87e6316b1e6cfc1ba2ade7225f0bb1a3a63e43a0ced927c135eb8bb48361375" "92ffa62354bf270b2526bf541eb43eb4a471bb8a3f4f3d2f67052079995069e5" "586e5bb76cc42e33684c69a12930df656d33c8d19dc7c6d5970fdd25ea069b42" "668c57f968cf52b9fed3f11c1c7fb2b0497e6f4c126ecc4e9dd3480b828af803" "d2ad04cdf7af01a803f0dd1b399f3cf0904688121f8a680f7a1af3935f4f1625" "9aeb193fa1626559e224e9a754777579a53ac71b4cddd2b8d2b6ef9adbb37754" default)))
+ '(fci-rule-character-color "#202020")
+ '(fci-rule-color "#202020")
  '(fringe-mode 5 nil (fringe))
  '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-style face trailing lines-tail) (require-final-newline . t) (whitespace-line-column . 80) (lexical-binding . t)))))
 ;_________________________________________________________________________
